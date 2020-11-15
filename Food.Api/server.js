9511edb4fe65345911fe8385043d8b9eb50a23e1
Geovani Brito <geovani.br@gmail.com>
2020-11-15 01:31:28 -0300

    Definição de rotas

--- Food.Api/server.js
+++ Food.Api/server.js
@@ -1,9 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+let pessoas = [];
 
 app.get('/', (req, res) => {
-    res.status(200).send('Olá Mundo!')
+    res.status(200).send(pessoas)
 })
+
+app.post('/', (req, res) => {
+    console.log('Corpo:', req.body);
+    pessoas.push(req.body);
+    res.status(201).send(req.body);
+})
+
+app.put('/:id', (req, res) => {
+    let pessoaEncotrada = pessoas.filter(pes => { 
+        return pes.id == req.params.id});
+    pessoaEncotrada = req.body;
+    res.status(202).send(pessoaEncotrada);
+})
+
+app.delete('/:id', (req, res) => {
+    for (let index = 0; index < pessoas.length; index++) {
+        const pessoa = pessoas[index]
+        if (pessoa.id == req.params.id) {
+            pessoas.splice(index, 1)
+        }
+    }
+    res.status(204).send();
+});
 
 app.listen(3000, () => {
     console.log("Servidor funcionando!")
